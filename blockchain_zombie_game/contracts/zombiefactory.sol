66ec77a9f939907ee7fcb6acf03a4cc26e6bc7f6
d3vyatk4ru <danya.devyatkin@mail.ru>
2022-08-10 16:51:07 +0300

    add zombie factory contract

---
+++ blockchain_zombie_game/contracts/zombiefactory.sol
@@ -0,0 +1,45 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.0;
+
+contract ZombieFactory {
+    
+    // event - специальная функция, вызов которой можно отследить во внешней среде, написанной на JS, например
+    event NewZombie(uint zombieId, string name, uint dna);
+
+    uint dnaDigits = 16;
+    uint dnaModulus = 10 ** dnaDigits;
+
+    struct Zombie {
+        string name;
+        uint dna;
+    }
+
+    Zombie[] public zombies;
+
+    mapping (uint => address) public zombieToOwner;
+    mapping (address => uint) ownerZombieCount;
+
+    function _createZombie(string _name, uint _dna) private {
+
+        // возвращает номер вставленного элемента + 1
+        uint id = zombies.push(Zombie(_name, _dna)) - 1;
+
+        zombieToOwner[id] = msg.sender;
+        ownerZombieCount[msg.sender]++;
+
+        NewZombie(id, _name, _dna);
+    }
+
+    function _generateRandomDna(string _str) private view returns (uint) {
+        uint rand = uint(keccak256(_str));
+        return rand % dnaModulus;
+    }
+
+    function createRandomZombie(string _name) public {
+
+        require(ownerZombieCount[msg.sender] == 0);
+
+        uint randDna = _generateRandomDna(_name);
+        _createZombie(_name, randDna);
+    }
+}
